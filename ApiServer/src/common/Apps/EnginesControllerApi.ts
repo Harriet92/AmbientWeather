24325c6b3ab70e35c53e9764b0de260916bb5dc3
Emilia Szymanska <szymanska.emilka@gmail.com>
2017-06-04 20:06:08 +0200

    Blinds script actual name

--- ApiServer/src/common/Apps/EnginesControllerApi.ts
+++ ApiServer/src/common/Apps/EnginesControllerApi.ts
@@ -2,14 +2,13 @@ var spawn = require('child_process').spawn;
 import { ChildProcess } from 'child_process';
 
 export default class EnginesControllerApi {
-    readonly openEnginesAppPath: string = "/var/piapps/openBlinds";
-    readonly closeEnginesAppPath: string = "/var/piapps/closeBlinds";
+    readonly enginesAppPath: string = "/var/piapps/spin";
 
     Open() {        
-        spawn(this.openEnginesAppPath);
+        spawn(this.enginesAppPath, ["800", "left"]);
     }
 
     Close() {
-        spawn(this.closeEnginesAppPath);
+        spawn(this.enginesAppPath, ["800", "right"]);
     }
 }
